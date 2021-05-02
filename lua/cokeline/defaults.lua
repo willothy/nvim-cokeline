fce1072be35ec278442718ce5d205662c30bc6a7
noib3 <riccardo.mazzarini@pm.me>
2021-05-02 23:00:05 +0200

    :sparkles: feat: Added option to configure the title format (still trying to get devicons to work). Plus now only the buffer's filename is listed instead of the whole path.

--- lua/cokeline/defaults.lua
+++ lua/cokeline/defaults.lua
@@ -1,6 +1,8 @@
 local defaults = {
+  use_devicons = false,
   hide_when_one_buffer = false,
-  colors = {
+  title_format = '{icon}{index}: {name} {flags}',
+  highlights = {
     cokeline = {bg='#3e4452'},
     buffers = {
       unfocused = {bg='#3e4452', fg='#abb2bf'},
--- lua/cokeline/init.lua
+++ lua/cokeline/init.lua
@@ -2,25 +2,45 @@ local augroups = require('cokeline/augroups')
 local defaults = require('cokeline/defaults')
 local highlights = require('cokeline/highlights')
 local utils = require('cokeline/utils')
+local has_devicons, devicons = pcall(require, 'nvim-web-devicons')
 local fn = vim.fn
 local M = {}
 
-local function get_state()
-  local state = {
-    buffers = {},
+local function cokeline(settings)
+  local titles = {}
+  local listed_buffers = fn.getbufinfo({buflisted=1})
+  local tags = {
+    icon = '{icon}',
+    index = '{index}',
+    name = '{name}',
+    flags = '{flags}',
   }
-  for i, buf in pairs(fn.getbufinfo({buflisted=1})) do
-    local is_focused = buf.bufnr == fn.bufnr('%')
-    local hl = (is_focused and 'CokeFocused') or 'CokeUnfocused'
-    local title = [[%#]]..hl..[[#]]..buf.name..[[%*]]
-    table.insert(state.buffers, title)
+  for index, buffer in ipairs(listed_buffers) do
+    local is_focused = buffer.bufnr == fn.bufnr('%')
+    local is_modified = vim.bo[buffer.bufnr].modified
+    local has_icon = has_devicons and settings.use_devicons
+    local has_flags = is_modified
+    local highlight = is_focused and 'CokeFocused' or 'CokeUnfocused'
+    local icon = ''
+    if has_icon then
+      local extension = fn.fnamemodify(buffer.name, ':e')
+      local raw_icon, icon_highlight =
+        devicons.get_icon(buffer.name, extension, {default=true})
+      icon = '%#'..icon_highlight..'#'..raw_icon..' %*'
+    end
+    local expands = {
+      icon = icon,
+      index = index,
+      name = fn.fnamemodify(buffer.name, ':t'),
+      flags = has_flags and ('['..(is_modified and '+' or '')..']') or ''
+    }
+    local title = '%#'..highlight..'# '..settings.title_format..' %*'
+    for k, v in pairs(tags) do
+      title = title:gsub(v, expands[k])
+    end
+    table.insert(titles, title)
   end
-  return state
-end
-
-function cokeline()
-  state = get_state()
-  return table.concat(state.buffers, ' | ')
+  return table.concat(titles, '')
 end
 
 -- function M.redraw()
@@ -35,7 +55,8 @@ end
 function M.setup(preferences)
   local settings = utils.update(defaults, preferences)
   augroups.setup(settings)
-  highlights.setup(settings.colors)
+  highlights.setup(settings.highlights)
+  function _G.cokeline() return cokeline(settings) end
   vim.o.tabline = '%!v:lua.cokeline()'
 end
 
