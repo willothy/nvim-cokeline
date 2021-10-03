096b41f0b1ef8237fc121dbefb17d0a8b11dd5ef
Neel Basak <neelfrost@gmail.com>
2021-10-03 12:33:04 +0530

    :bug: fix: `unique_prefix` on windows

--- lua/cokeline/buffers.lua
+++ lua/cokeline/buffers.lua
@@ -35,8 +35,10 @@ local M = {}
 local function get_unique_prefix(filename)
   -- Taken from github.com/famiu/feline.nvim
 
+  filename = filename:gsub('\\', '/')
+
   local listed_buffers = fn.getbufinfo({buflisted = 1})
-  local filenames = map(function(b) return b.name end, listed_buffers)
+  local filenames = map(function(b) return b.name:gsub('\\', '/') end, listed_buffers)
   local other_filenames = filter(
     function(f) return filename ~= f end,
     filenames
@@ -61,7 +63,7 @@ local function get_unique_prefix(filename)
     other_filenames
   )))
 
-  local path_separator = (fn.has('win32') == 0) and '/' or '\\'
+  local path_separator = '/'
 
   while index <= #filename do
     if filename:sub(index, index) == path_separator then
