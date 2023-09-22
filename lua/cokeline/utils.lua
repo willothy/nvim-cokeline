b56f12b9a72e96b1103accd6dd05b6e9f5cf44e4
Will Hopkins <willothyh@gmail.com>
2023-09-21 23:28:23 -0700

    refactor!: remove `utils.get_hex` - use `hlgroups.get_hl_attr`
    Hard deprecates `get_hex` by removing it entirely. It has been
    deprecated for a while, and causes issues with configs.

--- lua/cokeline/utils.lua
+++ lua/cokeline/utils.lua
@@ -1,17 +1,3 @@
-local vim_fn = vim.fn
-
----Returns the color set by the current colorscheme for the `attr` attribute of
----the `hlgroup_name` highlight group in hexadecimal format.
----@deprecated
----@param hlgroup_name  string
----@param attr  '"fg"' | '"bg"'
----@return string
-local get_hex = function(hlgroup_name, attr)
-  local hlgroup_ID = vim_fn.synIDtrans(vim_fn.hlID(hlgroup_name))
-  local hex = vim_fn.synIDattr(hlgroup_ID, attr)
-  return hex ~= "" and hex or "NONE"
-end
-
 ---@param bufnr number
 ---@param focus_next boolean
 ---@param wipeout boolean
@@ -135,6 +121,5 @@ local function buf_delete(bufnr, focus, wipeout)
 end
 
 return {
-  get_hex = get_hex,
   buf_delete = buf_delete,
 }
